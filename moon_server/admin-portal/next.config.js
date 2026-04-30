2a9376570adcb52f4767967e3a6da050bae08ee3
EN-rain <nieves.edriancit2011@gmail.com>
2026-04-30 19:49:57 +0800

    server.js fix

--- moon_server/admin-portal/next.config.js
+++ moon_server/admin-portal/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   env: {
     NEXT_PUBLIC_LOBBY_API_URL: process.env.NEXT_PUBLIC_LOBBY_API_URL,
     NEXT_PUBLIC_GAME_SERVER_URL: process.env.NEXT_PUBLIC_GAME_SERVER_URL,
